48aa5b02083d9d4565c3ea78e259881b075ba890
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-09-14 13:29:15 +0200

    Inicializando el proyecto

---
+++ src/main/java/com/dbg/IcM2Application.java
@@ -0,0 +1,12 @@
+package com.dbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcM2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcM2Application.class, args);
+	}
+}
